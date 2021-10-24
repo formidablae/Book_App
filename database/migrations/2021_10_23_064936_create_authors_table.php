45ee61c77c30c3bc8f27a72807aa4c0d97d4b8a0
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-24 10:29:53 +0200

    Created database migration tables

---
+++ database/migrations/2021_10_23_064936_create_authors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAuthorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('authors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email');
+            $table->string('github');
+            $table->string('twitter');
+            $table->string('location');
+            $table->string('latest_book_published');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('authors');
+    }
+}
