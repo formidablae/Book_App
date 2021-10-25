01a238680ae4575fb843c20ad901acaf1cbc598e
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-25 19:30:14 +0200

    Creating Author and Book models

---
+++ app/Book.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Author extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'author', 'ISBN', 'publishdate'
+    ];
+
+    /**
+     * The attributes excluded from the model's JSON form.
+     *
+     * @var array
+     */
+    protected $hidden = [];
+}
