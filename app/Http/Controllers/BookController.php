9a2f9fcd10d6b1ca82da0c43641b9c00c0e3b714
formidablae <81068781+formidablae@users.noreply.github.com>
2021-10-27 19:39:25 +0200

    Setted up Book and Author controller

---
+++ app/Http/Controllers/BookController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+
+    public function showAllBooks()
+    {
+        return response()->json(Book::all());
+    }
+
+    public function showOneBook($id)
+    {
+        return response()->json(Book::find($id));
+    }
+
+    public function create(Request $request)
+    {
+        $book = Book::create($request->all());
+
+        return response()->json($book, 201);
+    }
+
+    public function update($id, Request $request)
+    {
+        $book = Book::findOrFail($id);
+        $book->update($request->all());
+
+        return response()->json($book, 200);
+    }
+
+    public function delete($id)
+    {
+        Book::findOrFail($id)->delete();
+        return response('Deleted Successfully', 200);
+    }
+}
